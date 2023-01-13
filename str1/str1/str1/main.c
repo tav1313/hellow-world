8a2269b937f03a70c0f3c7cddd43d22382aa2a9d
atatarnikov <alexey.tatarnikov@gmail.com>
2023-01-13 20:02:03 +0700

    removing str1.xcodeproj

---
+++ str1/str1/str1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  str1
+//
+//  Created by Алексей Татарников on 13.01.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
