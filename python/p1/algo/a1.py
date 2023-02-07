a41b5e5f6d06b2dd9efa12c5b1764cd25d951938
atatarnikov <alexey.tatarnikov@gmail.com>
2023-02-07 10:46:22 +0700

    p1: sort_bubble1

--- python/p1/algo/a1.py
+++ python/p1/algo/a1.py
@@ -1,8 +1,8 @@
 def sort_bubble(lst):
-    for i in enumerate(lst):
-        print("iteration: ", i[0])
-        j = i[0]
-        while j < len(lst) - 1:
+    for i in range(len(lst) - 1):
+        print("iteration: ", i)
+        j = 0
+        while j < len(lst) - 1 - i:
             if lst[j] > lst[j + 1]:
                 tmp = lst[j]
                 lst[j] = lst[j + 1]
@@ -10,6 +10,13 @@ def sort_bubble(lst):
             j = j + 1
 
     return lst
+
+
+def sort_bubble1(lst):
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
 
 def search_linear(lst, value):
@@ -30,6 +37,11 @@ def print_hi(name):
     print("linear search idx: ", search_linear(lst, 55))
     sort_bubble(lst)
     print("bubbled list: ", lst)
+
+    lst = [9, 1, 6, 5, 3]
+    sort_bubble1(lst)
+    print("bubbled list1: ", lst)
+
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     lst = [1, 4, 5, 9]
     for i, v in enumerate(lst):
